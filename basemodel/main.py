f3f0bbd4fb18e8b3e2f25bcae6913a431d3151ac
tomoki819 <107849293+tomoki819@users.noreply.github.com>
2025-05-05 17:31:21 +0900

    ベースモデル作成のためにこちらを実行してください

--- basemodel/main.py
+++ basemodel/main.py
@@ -3,8 +3,6 @@ from generate import generate_output
 from evaluate_relevance import evaluate_relevance
 from huggingface_hub import notebook_login
 
-# Hugging Face Login（初回のみ必要）
-notebook_login()
 
 question = "磯村拓哉ユニットリーダーの論文「脳知能の三大理論を統合する三重等価性」の要約をしてください"
 
