e2873df5364891e3c6232fa50aed572fdcc3e401
tomoki819 <107849293+tomoki819@users.noreply.github.com>
2025-05-05 17:46:31 +0900

    huggingfaceの認証です。最初にやりましょう。

--- config/config.py
+++ config/config.py
@@ -1,12 +1,4 @@
-import torch
-import random
 from huggingface_hub import notebook_login
-
-# シード固定
-random.seed(0)
-
-# CUDA or CPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # Hugging Face Login（初回のみ必要）
 notebook_login()
+
